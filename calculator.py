4b6733158dd81fbc13c7da16d0fe98875f9ca765
Mallard-Dash <mjukcent@gmail.com>
2025-10-18 09:23:19 +0200

    Added testing

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+def add(a, b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b == 0:
+        raise ValueError ("Division by zero")
+    return a / b
+
+#TODO: implement a divide() function later
+
